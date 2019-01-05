f10096523848032e49ffa9e35438395bc756b2fc
Simon Arlott <sa.me.uk>
2019-01-05 21:54:58 +0000

    Finish availability calculation

--- availability.py
+++ availability.py
@@ -15,6 +15,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
+from datetime import datetime, timedelta, timezone
 import argparse
 import collections
 import csv
@@ -33,19 +34,47 @@ class Availability(enum.Enum):
 	Maybe = 2
 	Yes = 3
 
-players_required = 4
+weekdays = dict(zip(['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'], range(1, 8)))
+
+
+"""Cache time zone conversions for a given datetime"""
+class Timestamp:
+	def __init__(self, dt):
+		self.dt = dt
+		self.dt_tz = {}
+
+	def astimezone(self, timezone):
+		if timezone.zone not in self.dt_tz:
+			self.dt_tz[timezone.zone] = self.dt.astimezone(timezone)
+		return self.dt_tz[timezone.zone]
 
 
 class PlayerAvailability:
 	def __init__(self):
-		self.times = set()
+		self.periods = set()
 
 	def add(self, time_zone, day, time_from, time_to, available):
-		self.times.add(AvailablePeriod(time_zone, day, time_from, time_to, available))
+		self.periods.add(AvailablePeriod(pytz.timezone(time_zone), weekdays[day], time_from, time_to, available))
 
 	def available_at(self, ts):
-		# TODO
-		return Availability.No
+		available = Availability.No
+
+		for period in self.periods:
+			player_ts = ts.astimezone(period.time_zone)
+			if player_ts.isoweekday() != period.day:
+				continue
+			if player_ts.hour < period.time_from[0]:
+				continue
+			if player_ts.hour > period.time_to[0]:
+				continue
+			if player_ts.hour == period.time_from[0] and player_ts.minute < period.time_from[1]:
+				continue
+			if player_ts.hour == period.time_to[0] and player_ts.minute >= period.time_to[1]:
+				continue
+			if period.available.value > available.value:
+				available = period.available
+
+		return available
 
 
 class TeamAvailability:
@@ -55,8 +84,8 @@ class TeamAvailability:
 	def add(self, player, time_zone, day, time_from, time_to, available):
 		self.players[player].add(time_zone, day, time_from, time_to, available)
 
-	def available_at(self, ts):
-		available = collections.Counter(Availability.__members__.values())
+	def available_at(self, ts, players_required):
+		available = collections.Counter()
 		for player in self.players.values():
 			player_availability = player.available_at(ts)
 			# Count them in all availability levels up to the best one they have
@@ -66,10 +95,10 @@ class TeamAvailability:
 					available[availability] += 1
 
 		# Find the best availability status that has the required number of players
-		result = (Availability.No, 0)
+		result = (0, Availability.No)
 		for availability in Availability.__members__.values():
 			if available[availability] >= players_required:
-				result = (availability, available[availability])
+				result = (available[availability], availability)
 		return result
 
 
@@ -109,7 +138,7 @@ class LeagueAvailability:
 						raise Exception(f"Invalid player on line {line}")
 					if time_zone not in pytz.all_timezones_set:
 						raise Exception(f"Invalid time zone on line {line}: {time_zone}")
-					if day not in ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]:
+					if day not in weekdays.keys():
 						raise Exception(f"Invalid day on line {line}: {day}")
 
 					(last_team, last_player, last_time_zone, last_day, last_available) = (team, player, time_zone, day, available)
@@ -135,30 +164,103 @@ class LeagueAvailability:
 			if not header:
 				raise Exception("Unable to find header")
 
-	def available_at(self, ts):
+	def available_at(self, ts, players_required):
 		teams = {}
 
 		for name, team in self.teams.items():
-			(availability, players) = team.available_at(ts)
+			(players, availability) = team.available_at(ts, players_required)
 			if availability.value > Availability.No.value:
 				teams[name] = (players, availability)
 
 		return teams
 
 
+def generate_output(args, output=sys.stdout, time_zones={
+			"UTC": (pytz.timezone("UTC"), "%d/%m"),
+			"CET/CEST": (pytz.timezone("Europe/Paris"), "%d/%m"),
+			"Pacific": (pytz.timezone("US/Pacific"), "%m/%d"),
+			"Mountain": (pytz.timezone("US/Mountain"), "%m/%d"),
+			"Eastern": (pytz.timezone("US/Eastern"), "%m/%d"),
+		}):
+	league = LeagueAvailability(args.filename)
+
+	now = datetime.now(timezone.utc).replace(hour=0, minute=0, second=0, microsecond=0)
+	end = now + timedelta(weeks=args.weeks)
+	dt = now
+
+	last_teams = {}
+	team_player_minimums = {}
+	team_player_maximums = {}
+	last_from = None
+
+	rows = list(time_zones.keys()) + list(sorted(league.teams.keys()))
+	csvfile = csv.DictWriter(output, rows, quoting=csv.QUOTE_ALL)
+	csvfile.writeheader()
+
+	while dt < end:
+		teams = league.available_at(Timestamp(dt), args.players)
+		if __summarise_teams(teams) != __summarise_teams(last_teams):
+			if last_teams:
+				__output_team_list(csvfile, time_zones, last_from, dt, last_teams, team_player_minimums, team_player_maximums)
+			last_from = dt
+			team_player_minimums = {}
+			team_player_maximums = {}
+
+		if teams and not last_teams:
+			last_from = dt
+			team_player_minimums = {}
+			team_player_maximums = {}
+		last_teams = teams
+		for (team, (players, availability)) in teams.items():
+			if team_player_minimums.get(team, sys.maxsize) > players:
+				team_player_minimums[team] = players
+			if team_player_maximums.get(team, 0) < players:
+				team_player_maximums[team] = players
+
+		dt += timedelta(minutes=1)
+
+		if dt == end:
+			if last_teams:
+				__output_team_list(csvfile, time_zones, last_from, dt, last_teams, team_player_minimums, team_player_maximums)
+
+
+def __summarise_teams(teams):
+	return set([(team, availability) for (team, (players, availability)) in teams.items()])
+
+
+def __output_team_list(csvfile, time_zones, dt_from, dt_to, teams, team_player_minimums, team_player_maximums):
+	row = {}
+
+	for (zone_name, (time_zone, date_format)) in time_zones.items():
+		dt_from_tz = dt_from.astimezone(time_zone)
+		dt_to_tz = dt_to.astimezone(time_zone)
+
+		from_str = dt_from_tz.strftime(f"%a {date_format} %H:%M")
+		if dt_from_tz.date() == dt_to_tz.date():
+			to_str = dt_to_tz.strftime(f"%H:%M")
+		else:
+			to_str = dt_to_tz.strftime(f"%a {date_format} %H:%M")
+
+		row[zone_name] = f"{from_str} to {to_str}"
+
+	for (team, (players, availability)) in teams.items():
+		if team_player_minimums[team] == team_player_maximums[team]:
+			team_str = f"{players}"
+		else:
+			team_str = f"{team_player_minimums[team]}-{team_player_maximums[team]}"
+
+		if availability == availability.Yes:
+			row[team] = f"'{team_str}"
+		else:
+			row[team] = f"'({team_str})"
+
+	csvfile.writerow(row)
+
+
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Process availability")
 	parser.add_argument("filename", metavar="FILENAME", type=str, help="CSV file containing availability data")
+	parser.add_argument("-w", "--weeks", type=int, default=4, help="Number of weeks to output")
+	parser.add_argument("-p", "--players", type=int, default=4, help="Minimum number of players from each team")
 	args = parser.parse_args()
-
-	league = LeagueAvailability(args.filename)
-
-	for team_name, team in league.teams.items():
-		team.available_at(None)
-		print(team_name)
-		for player_name, player in team.players.items():
-			print(player_name)
-			for time in player.times:
-				print(time)
-
-	# TODO
+	generate_output(args)
